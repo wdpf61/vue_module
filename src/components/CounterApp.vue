f0182903feeb3cfc28d1fba0aaad0d4146563e37
batch61 <ccsl61588@gmail.com>
2025-03-20 10:18:50 +0600

    computed watch

--- src/components/CounterApp.vue
+++ src/components/CounterApp.vue
@@ -1,14 +1,40 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref, watch } from "vue";
+
+let firstName= ref("Habib")
+let lastName= ref("Hasan")
+
 
 let count= ref(0);
 const increase = ()=> count.value ++ ;
 const decrease = ()=> count.value -- ;
 
 
+let fullName= computed(()=>{
+     return firstName.value +" "+ lastName.value
+})
+
+
+const fullName2= ()=> firstName.value +" "+ lastName.value;
+
+  watch(count, (prev, current)=>{
+     console.log(prev);
+     console.log(current);
+  })
+
 </script>
 
 <template>
+ 
+  <h1>First Name : {{ firstName }}</h1>
+  <h1>Last Name : {{ lastName }}</h1>
+  <h1>Full Name : {{ fullName }}</h1>
+  <h1>Full Name 2: {{ fullName2() }}</h1>
+
+  <input type="text" v-model="firstName" /> <br>
+  <input type="text" v-model="lastName" />
+
+
 <h1>Counter</h1>
 <p>Value {{ count }} </p>
 <button @click="decrease" >Minus</button>
