f1cdc246f83665aa2aa0f009bcaf458dc07e6122
batch61 <ccsl61588@gmail.com>
2025-03-23 10:53:32 +0600

    sss

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      not found 
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Contact from '@/pages/Contact.vue';
 import Dashboard from '@/pages/Dashboard.vue';
+import NotFound from '@/pages/NotFound.vue';
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 
@@ -8,6 +9,7 @@ const routes = [
   { path: '/', component: Dashboard },
   { path:'/contact', component: Contact },
   { path: '/foo', component: Contact },
+  { path: '/:pathMatch(.*)*', component: NotFound },
  
 ]
 
